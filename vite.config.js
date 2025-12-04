6f8053c33578bab6b3f1319d5ed74c8c019d9bd5
plovk <deminayoy@gmail.com>
2025-12-04 12:01:10 +0300

    change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base:  '/technology-tracker-main-2/',
+    base:  '/technology-tracker-clean/',
 })
